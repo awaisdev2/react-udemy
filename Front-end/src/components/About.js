627a6fa4a770da9121d89578e8ecc19321b62802
Awais <awaisdev2@gmail.com>
2023-09-24 14:25:28 +0500

    about page

--- Front-end/src/components/About.js
+++ Front-end/src/components/About.js
@@ -16,29 +16,29 @@ export default function About() {
             <h2 class="text-2xl font-semibold mb-4">Our Team</h2>
             <div class="flex flex-wrap -mx-4">
                 <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 px-4 mb-4">
-                    <div class="bg-gray-200 p-4 rounded-lg">
-                        <img src="https://via.placeholder.com/150" alt="Team Member" class="w-20 h-20 mx-auto mb-2 rounded-full" />
+                    <div class="bg-gray-200 text-center p-4 rounded-lg">
+                        <img src="https://o.remove.bg/uploads/4d823b97-01a6-4d88-95d4-1a6d59b6e6e6/300-23.jpg" alt="Team Member" class="w-20 h-20 mx-auto mb-2 rounded-full" />
                         <h3 class="text-lg font-semibold">Azhan Malik</h3>
                         <p class="text-gray-700">Designer</p>
                     </div>
                 </div>
                 <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 px-4 mb-4">
-                    <div class="bg-gray-200 p-4 rounded-lg">
-                        <img src="https://via.placeholder.com/150" alt="Team Member" class="w-20 h-20 mx-auto mb-2 rounded-full" />
+                    <div class="bg-gray-200 text-center p-4 rounded-lg">
+                        <img src="https://o.remove.bg/uploads/179ebc24-2f5c-45d9-9adf-78be073de80f/300-29.jpg" alt="Team Member" class="w-20 h-20 mx-auto mb-2 rounded-full" />
                         <h3 class="text-lg font-semibold">Awais Malik</h3>
                         <p class="text-gray-700">Developer</p>
                     </div>
                 </div>
                 <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 px-4 mb-4">
-                    <div class="bg-gray-200 p-4 rounded-lg">
-                        <img src="https://via.placeholder.com/150" alt="Team Member" class="w-20 h-20 mx-auto mb-2 rounded-full" />
+                    <div class="bg-gray-200 text-center p-4 rounded-lg">
+                        <img src="https://o.remove.bg/uploads/421cebc3-6f99-49c7-9fcd-d9e7801c52fc/300-13.jpg" alt="Team Member" class="w-20 h-20 mx-auto mb-2 rounded-full" />
                         <h3 class="text-lg font-semibold">Ali Ahmad</h3>
                         <p class="text-gray-700">Marketing</p>
                     </div>
                 </div>
                 <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 px-4 mb-4">
-                    <div class="bg-gray-200 p-4 rounded-lg">
-                        <img src="https://via.placeholder.com/150" alt="Team Member" class="w-20 h-20 mx-auto mb-2 rounded-full" />
+                    <div class="bg-gray-200 text-center p-4 rounded-lg">
+                        <img src="https://o.remove.bg/uploads/445f251b-4f04-4594-80f5-c596315b29dc/300-15.jpg" alt="Team Member" class="w-20 h-20 mx-auto mb-2 rounded-full" />
                         <h3 class="text-lg font-semibold">HNTECHS</h3>
                         <p class="text-gray-700">Support</p>
                     </div>
